fb53841a861e9ebecd31c3ca9fb045fa0537653c
Avermoal <misho54@mail.ru>
2025-10-15 08:10:07 +0600

    Second commit

---
+++ includes/Gui/window.h
@@ -0,0 +1,6 @@
+#ifndef GUI_WINDOW_H
+#define GUI_WINDOW_H
+
+
+
+#endif/*GUI_WINDOW_H*/
---
+++ includes/application.h
@@ -0,0 +1,11 @@
+#ifndef APPLICATION_H
+#define APPLICATION_H
+
+#include "window.h"
+
+
+
+
+
+
+#endif/*APPLICATION_H*/
